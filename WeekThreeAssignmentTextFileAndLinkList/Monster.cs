c28d17809dff67631b3071405e51296fb180acb8
Gursimar Virdi <385491@colum.edu>
2023-09-27 21:59:09 -0500

    Add project files.

---
+++ WeekThreeAssignmentTextFileAndLinkList/Monster.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekThreeAssignmentTextFileAndLinkList
+{
+    class Monster
+    {
+        public string Type { get; set; }
+        public int HP { get; set; } 
+        public int MP { get; set; }
+        public int AP { get; set; }
+        public int DEF { get; set; }
+        public bool IsAlive { get { return HP > 0; } }
+
+        public Monster(string type, int hp, int mp, int ap, int def)
+        {
+            Type = type;
+            HP = hp;
+            MP = mp;
+            AP = ap;
+            DEF = def;
+        }
+
+        public void Attack(Monster target)
+        {
+            int damage = Math.Max(0, AP - target.DEF);
+            target.HP -= damage;
+            Console.WriteLine($"{Type} attacks {target.Type} for {damage} damage!");
+        }
+    }
+}
---
+++ WeekThreeAssignmentTextFileAndLinkList/Program.cs
@@ -0,0 +1,99 @@
+﻿/*
+    Made by: Gursimar Virdi
+    Date: September 27th, 2023
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WeekThreeAssignmentTextFileAndLinkList
+{
+    class Program 
+    {
+        static List<Monster> LoadMonsters(string Stats)
+        {
+            //Lists out the monsters that is being read from the file
+            List<Monster> monsters = new List<Monster>();
+            using (StreamReader reader = new StreamReader(Stats))
+            {
+                //This reads the file with the stats of the monster and adds it to the list
+                reader.ReadLine();
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] data = line.Split(' ');
+                    string type = data[0];
+                    int hp = int.Parse(data[1]);
+                    int mp = int.Parse(data[2]);
+                    int ap = int.Parse(data[3]);
+                    int def = int.Parse(data[4]);
+                    monsters.Add(new Monster(type, hp, mp, ap, def));
+                }
+            }
+            return monsters;
+        }
+
+        static void Main(string[] args)
+        {
+            //This is list of the monster that
+            List<Monster> monsters = LoadMonsters("Stats.txt");
+            List<Monster> restofMonsters = monsters.ToList();
+            
+            //These are the stats of the player
+            int playerHP = 100;
+            int playerAP = 30;
+
+            Random random = new Random();
+            //Beginning text to enter the game
+            Console.WriteLine("Welcome to the Slither Lockup Game");
+
+            while (restofMonsters.Count > 0 && playerHP > 0) 
+            {
+                //This displays the monster being encountered in the room that the player is in, it randomize which monster you are fighting in the room
+                Monster currentMonster = restofMonsters[random.Next(restofMonsters.Count)];
+                Console.WriteLine($"You enter a room and encounter a {currentMonster.Type}!");
+
+                while (currentMonster.IsAlive && playerHP > 0)
+                {
+                    //This displays the amount of HP the player and monster have, as well as display which monster is it
+                    Console.WriteLine($"Your HP: {playerHP}, {currentMonster.Type}'s HP: {currentMonster.HP}");
+                    // The monster attack the player
+                    currentMonster.Attack(currentMonster); 
+
+                    if (currentMonster.IsAlive)
+                    {
+                        //This is the shows how the player takes damage then will subtract the amount of damage from the health of player
+                        int damage = Math.Max(2, currentMonster.AP - playerHP);
+                        playerHP -= damage;
+                        Console.WriteLine($"{currentMonster.Type} attacks you and has dealt {damage} damage!");
+                    }
+                }
+
+                if (!currentMonster.IsAlive) 
+                {
+                    //This is the text if the player defeats a monster in the dungeon
+                    Console.WriteLine($"You have defeated {currentMonster.Type}!");
+                    restofMonsters.Remove(currentMonster);
+                }
+                else
+                {
+                    //This is the text if the player has been killed by one of the three monsters
+                    Console.WriteLine($"You have died to {currentMonster.Type }!");
+                    break;
+                }
+            }
+
+            if (playerHP <= 0)
+            {
+                //This is the end text if the player has died from a monster
+                Console.WriteLine("Game Over, You Died!!!");
+            }
+            else
+            {
+                //This is the end text if the player killed all the monsters
+                Console.WriteLine("Congrats! You killed all the monster in the dungeon!");
+            }
+        }
+    }
+}
